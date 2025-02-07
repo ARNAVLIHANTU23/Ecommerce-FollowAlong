a960a09e3f51fa23592e433122d021e907226230
ARNAVLIHANTU23 <Arnav.Lihantu@kalvium.community>
2025-02-07 12:11:18 +0530

    Milestone 10,11,12

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,5 +1,6 @@
 import LoginPage from "./pages/Login";
 import SignupPage from "./pages/Signup";
 import Home from "./pages/Home";
-import CreateProduct from "./pages/Product.jsx";
-export {LoginPage, SignupPage, Home, CreateProduct};
+import CreateProduct from "./pages/Create_Product.jsx";
+import MyProducts from "./pages/myProducts.jsx";
+export {LoginPage, SignupPage, Home, CreateProduct, MyProducts};
